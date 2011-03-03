df47724293693b409f9aa0ec48f15d4f932009b6
Gk <genkiwow@gmail.com>
2011-03-03 03:24:50 -0800

    Edited init.rb via GitHub

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 require_dependency 'hooks/view_layouts_base_html_head_hook'
 
-Redmine::Plugin.register :redmine_light_box do
+Redmine::Plugin.register :redmine_lightbox do
   name 'Redmine Light Box plugin'
   author 'G.K.'
   description 'Lightbox for attachments'
