5ab3b415f6b70f6ca1cd2ea5b105ae3e6956dbc9
Zipme <genkiwow@gmail.com>
2011-03-05 15:18:16 +0100

    add support for wiki and board page

--- lib/hooks/view_layouts_base_html_head_hook.rb
+++ lib/hooks/view_layouts_base_html_head_hook.rb
@@ -2,9 +2,9 @@ module RedmineLightBox
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        if context[:controller] && (
-                                    context[:controller].is_a?(IssuesController) 
-                                    )
+        if context[:controller] && (  context[:controller].is_a?(IssuesController) || 
+                                      context[:controller].is_a?(WikiController) ||
+                                      context[:controller].is_a?(BoardsController))
           return stylesheet_link_tag("jquery.fancybox-1.3.4.css", :plugin => "redmine_lightbox", :media => "screen") +
             stylesheet_link_tag("lightbox.css", :plugin => "redmine_lightbox", :media => "screen") +
             javascript_include_tag('http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js') +
