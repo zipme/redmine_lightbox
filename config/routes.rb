42cefa68bf08c0b1cd56e05eb732b5863f243306
Daniel Munn <dan.munn@munnster.co.uk>
2012-06-06 12:24:52 +0100

    Redmine 1.3 and 2.0 compatible routes; file cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
-RedmineApp::Application.routes.draw do
-  get 'attachments/download_inline/:id/:filename', :controller => 'attachments', :action => 'download_inline', :id => /\d+/, :filename => /.*/
+if Gem::Version.new("3.0") > Gem::Version.new(Rails.version) then
+  #Redmine 1.x
+  ActionController::Routing::Routes.draw do |map|
+    map.connect 'attachments/download_inline/:id/:filename', :controller => 'attachments', :action => 'download_inline', :id => /\d+/, :filename => /.*/
+  end
+
+else
+  #Redmine 2.x
+  RedmineApp::Application.routes.draw do
+    get 'attachments/download_inline/:id/:filename', :controller => 'attachments', :action => 'download_inline', :id => /\d+/, :filename => /.*/
+  end
 end
