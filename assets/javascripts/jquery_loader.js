76eb175fb4207febe15294cfa5ad543a964a574f
Vladimir <nettsundere@gmail.com>
2012-09-21 15:35:00 +0400

    Added jquery_loader

---
+++ assets/javascripts/jquery_loader.js
@@ -0,0 +1,6 @@
+if(typeof jQuery == 'undefined') {
+  document.write('<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>');
+}
+else {
+  jQuery.noConflict();
+}
--- lib/hooks/view_layouts_base_html_head_hook.rb
+++ lib/hooks/view_layouts_base_html_head_hook.rb
@@ -2,18 +2,17 @@ module RedmineLightBox
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        if context[:controller] && (  context[:controller].is_a?(IssuesController) || 
+        if context[:controller] && (  context[:controller].is_a?(IssuesController) ||
                                       context[:controller].is_a?(WikiController) ||
                                       context[:controller].is_a?(DocumentsController) ||
-                                      context[:controller].is_a?(FilesController) || 
+                                      context[:controller].is_a?(FilesController) ||
                                       context[:controller].is_a?(BoardsController))
-          return stylesheet_link_tag("jquery.fancybox-1.3.4.css", :plugin => "redmine_lightbox", :media => "screen") +
-            stylesheet_link_tag("lightbox.css", :plugin => "redmine_lightbox", :media => "screen") +
-            javascript_include_tag('//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js') +
-            javascript_tag('jQuery.noConflict();') +
-            javascript_include_tag('jquery.fancybox-1.3.4.pack.js', :plugin => 'redmine_lightbox') +
-            javascript_include_tag('jquery.easing-1.3.pack.js', :plugin => 'redmine_lightbox') +            
-            javascript_include_tag('lightbox.js', :plugin => 'redmine_lightbox')
+          return stylesheet_link_tag("jquery.fancybox-1.3.4.css", :plugin => "redmine_lightbox", :media => "screen") \
+            + stylesheet_link_tag("lightbox.css", :plugin => "redmine_lightbox", :media => "screen") \
+            + javascript_include_tag('jquery_loader.js', :plugin => 'redmine_lightbox') \
+            + javascript_include_tag('jquery.fancybox-1.3.4.pack.js', :plugin => 'redmine_lightbox') \
+            + javascript_include_tag('jquery.easing-1.3.pack.js', :plugin => 'redmine_lightbox') \
+            + javascript_include_tag('lightbox.js', :plugin => 'redmine_lightbox') \
         else
           return ''
         end
