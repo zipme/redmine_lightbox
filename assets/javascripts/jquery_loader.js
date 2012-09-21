27f3b9eb35d96f2677f12ead3086ab3e0f219b9e
Vladimir <nettsundere@gmail.com>
2012-09-21 19:39:32 +0400

    Fixed jquery_loader

--- assets/javascripts/jquery_loader.js
+++ assets/javascripts/jquery_loader.js
@@ -1,6 +1,7 @@
 if(typeof jQuery == 'undefined') {
-  document.write('<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>');
+  document.write('<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>')
+  document.write('<script type="text/javascript">jQuery.noConflict();</script>')
 }
 else {
-  jQuery.noConflict();
+  jQuery.noConflict()
 }
