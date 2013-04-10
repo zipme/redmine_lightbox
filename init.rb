60a5373e96f467b3cd9e55de0bbc780a4a1878e7
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-04-10 16:50:46 +0400

    bumped version to 0.0.4

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_lightbox do
   name 'Redmine Light Box plugin'
   author 'G.K., Undev'
   description 'Lightbox for attachments'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/Undev/redmine_lightbox'
   author_url 'https://github.com/Undev'
 
